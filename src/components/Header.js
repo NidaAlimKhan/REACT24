571f3fd24bbcb0d313b85b69e6782f5bacd6f9ca
NidaAlimKhan <nidaalimkhan@gmail.com>
2024-12-27 21:24:01 +0500

    done

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>HEALTH INFORMATION WITH DR. NIDA ALIM KHAN</h1>
+      <p>Hello Everyone!
+        <br />
+        <br />
+        Thank you for showing interest, This Website is all about sharing information about healthy lifestyles, beauty, skincare, daily routine, exercise, and happiness.
+         I strongly believe that food and happiness are the biggest medicines.
+        Stay connected for more healthy information.
+        <br/>
+        <br/>
+        My pleasure.</p>
+    </div>
+  );
+};
+
+export default Header;
